b84addf1b3fe62270d2218a0adb84ac1b581a508
Mike Semko <msemko@gmail.com>
2020-02-29 15:12:37 -0500

    enable react

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import rootStyles from "./test.module.scss";
+
+const Root = () => (
+  <div className={rootStyles.bluetext}>
+    React works
+  </div>
+);
+
+export default Root;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,8 @@
-import Test2 from "./test";
-import "./test.module.scss";
-const test = async () => {
-  return await 2;
-};
+import { render } from "react-dom";
+import React from "react";
+import Root from "./../../app";
 
-import tieDefender from "../img/tie_defender.png";
-
-test().then(value => console.log(value));
-
-console.log("this works2");
-
-const obj = {
-  a: 32,
-  b: 46
-};
-
-const obj2 = {
-  ...obj,
-  b: 999,
-  c: "hello"
-};
-
-console.table(obj2);
-
-function decorator() {
-  return function(cl) {
-    return class extends cl {
-      y = 66;
-      func2() {
-        return `${this.a} ${this.y}`;
-      }
-    };
-  };
-}
-
-@decorator()
-class Test {
-  a = 34;
-  func() {
-    return this.a;
-  }
-}
-
-const t = new Test();
-
-console.log(t.func());
-console.log(t.func2());
-
-const tt2 = new Test2;
-
-tt2.foo();
-
-<div></div>;
+render(<Root />, document.getElementById("approot"));
 
 if (module.hot) {
   module.hot.accept();
