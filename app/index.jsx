6a12cc834047e102254d05bd50992fa4b0348005
Mike Semko <msemko@gmail.com>
2020-03-01 00:17:01 -0500

    Img src works seamlessly without requiring import

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import rootStyles from "./test.module.scss";
 const Root = () => (
   <div className={rootStyles.bluetext}>
     React works
+    <img src="../assets/img/tie_defender.png" alt=""/>
   </div>
 );
 
